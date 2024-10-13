186ec9695c26e027f4bf623f2d3f8a0cd401bad0
Relzarick <156683271+Relzarick@users.noreply.github.com>
2024-10-13 21:01:19 +0800

    Add files via upload

--- page.js
+++ page.js
@@ -1,3 +1,7 @@
 const box1 = document.getElementById("box-");
 const box2 = document.getElementById("box-2");
 const box3 = document.getElementById("box-3");
+
+box1.addEventListener("click", () => {});
+box2.addEventListener("click", () => {});
+box3.addEventListener("click", () => {});
