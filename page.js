595dc2d6648d6109cceb78075a1c417dade4b739
Relzarick <156683271+Relzarick@users.noreply.github.com>
2024-11-10 17:58:57 +0800

    Add files via upload

--- page.js
+++ page.js
@@ -5,12 +5,53 @@ const ytContainerText = document.getElementById("project-container-info-a");
 const spContainerText = document.getElementById("project-container-info-b");
 const fccContainerText = document.getElementById("project-container-info-c");
 
+const burgerMenu = document.getElementById("burger-menu");
+const dropDown = document.getElementById("nav-dropdown-menu");
+
 let toggled = false;
-const containerEventAction = (container) => {
-  toggled = !toggled;
-  container.style.display = toggled ? "none" : "block";
+
+const renderKeys = (keys) => {
+  const key = keys;
+  const renderItems = [
+    { ytBox: "ytContainerText" },
+    { spBox: "spContainerText" },
+    { fccBox: "fccContainerText" },
+    { dropDown: "dropDown" },
+  ];
+
+  const foundItem = renderItems.find((item) => item.hasOwnProperty(key));
+  const containerDom = foundItem[key];
+  // containerDom doesnt === the dom element
+  console.log(containerDom === ytContainerText);
+
+  if (foundItem) {
+    containerDom.style.display = "none";
+    console.log(containerDom);
+  }
 };
 
-ytContainerText.addEventListener("click", () => containerEventAction(ytBox));
-spContainerText.addEventListener("click", () => containerEventAction(spBox));
-fccContainerText.addEventListener("click", () => containerEventAction(fccBox));
+const renderContainerItems = (key) => {
+  renderKeys(key);
+};
+
+const containerEventAction = (container, key) => {
+  toggled = !toggled;
+  container.style.display = toggled ? "none" : "block";
+  renderContainerItems(key);
+};
+
+const navDisplay = () => {};
+
+ytContainerText.addEventListener("click", () =>
+  containerEventAction(ytBox, "ytBox")
+);
+spContainerText.addEventListener("click", () =>
+  containerEventAction(spBox, "spBox")
+);
+fccContainerText.addEventListener("click", () =>
+  containerEventAction(fccBox, "fccBox")
+);
+
+burgerMenu.addEventListener("click", () =>
+  containerEventAction(dropDown, "dropDown")
+);
