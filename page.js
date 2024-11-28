270b7d99e876a30c537c5e110ad9e48098213a2c
Relzarick <156683271+Relzarick@users.noreply.github.com>
2024-11-28 20:42:08 +0800

    Add files via upload

--- page.js
+++ page.js
@@ -5,7 +5,9 @@ const ytContainerText = document.getElementById("project-container-info-a");
 const spContainerText = document.getElementById("project-container-info-b");
 const fccContainerText = document.getElementById("project-container-info-c");
 
-const burgerMenu = document.getElementById("burger-menu");
+const burgerMenu = document.querySelector(".burger-menu");
+const navContents = document.querySelectorAll(".js-navbar");
+const navContainer = document.querySelector(".nav-div-container");
 
 //
 const containerDisplay = (key, num) => {
@@ -13,20 +15,20 @@ const containerDisplay = (key, num) => {
     { ytBox: "project-container-info-a", num: 0 },
     { spBox: "project-container-info-b", num: 1 },
     { fccBox: "project-container-info-c", num: 2 },
-    { burgerMenu: "burger-menu" },
+    { burgerMenu: "burger-menu", num: 3 },
   ];
 
   const foundItem = domKeys.find((item) => item.hasOwnProperty(key));
-  console.log(foundItem[key]);
+  //console.log(foundItem[key]);
   const containerDom = document.getElementById(foundItem[key]);
   const containerNum = domKeys[num].num;
-  console.log(containerDom);
-
-  // need nav to be seperate from this
+  //console.log(containerNum);
 
   if (containerNum <= 2) {
     containerDom.style.display = "none";
     elementAppend(containerNum);
+  } else {
+    navDisplay();
   }
 };
 
@@ -46,11 +48,16 @@ const elementAppend = (num) => {
 };
 
 //
-const navDisplay = () => {};
+const navDisplay = () => {
+  navContents.forEach((elem) => {
+    elem.classList.toggle("nav-toggle");
+  });
+  navContainer.classList.toggle("nav-background");
+  burgerMenu.classList.toggle("burger-filter");
+};
 
 const renderContainerItems = (key, num) => {
   containerDisplay(key, num);
-  //navDisplay();
 };
 
 const containerEventAction = (container, key, num) => {
@@ -71,6 +78,9 @@ fccContainerText.addEventListener("click", () =>
   containerEventAction(fccBox, "fccBox", 2)
 );
 
-burgerMenu.addEventListener("click", () =>
-  containerEventAction(burgerMenu, "burgerMenu")
+burgerMenu.addEventListener("mouseover", () =>
+  containerEventAction(burgerMenu, "burgerMenu", 3)
 );
+// navContainer.addEventListener("mouseout", () =>
+//   containerEventAction(burgerMenu, "burgerMenu", 3)
+// );
