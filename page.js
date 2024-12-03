939fc13d4e07e90f972f484a62bcdfafda7e7617
Relzarick <156683271+Relzarick@users.noreply.github.com>
2024-12-03 21:31:42 +0800

    Add files via upload

--- page.js
+++ page.js
@@ -8,9 +8,58 @@ const fccContainerText = document.getElementById("project-container-info-c");
 const burgerMenu = document.querySelector(".burger-menu");
 const navContents = document.querySelectorAll(".js-navbar");
 const navContainer = document.querySelector(".nav-div-container");
+const navSubContainer = document.querySelector(".nav-div-subcontainer");
 
 //
-const containerDisplay = (key, num) => {
+
+const elementAppend = (num) => {
+  const containerTexts = [
+    {
+      pointOne:
+        "Gained experience in creating layout structures with HTML and organizing content.",
+      pointTwo:
+        "Developed CSS styling skills, including color, font, and element positioning.",
+      pointThree:
+        "Enhanced problem-solving by debugging and refining code for design and functionality.",
+    },
+    {
+      pointOne:
+        "Learned basics of JavaScript syntax, including variables, data types, and operators.",
+      pointTwo: "Learned about scope and variable declaration ",
+      pointThree:
+        "Gained an understanding of functions and how to create and call them.",
+    },
+    {
+      pointOne:
+        "Became familiar with scope, closures, and how they affect variable access and function execution.",
+      pointTwo:
+        "Explored objects and arrays in-depth, understanding how to store and manipulate data.",
+      pointThree:
+        "Learned how to work with JavaScript events, handling user interactions like clicks and keyboard inputs.",
+    },
+  ];
+
+  //console.log(containerTexts[0].pointOne);
+
+  const parentElement = document.querySelectorAll(".sub-container-3-box");
+  //console.log(parentElement[num]);
+  parentElement[num].innerHTML += `<ul class= "bullet-points">
+  <li class= "points">${containerTexts[num].pointOne}</li>
+  <li class= "points">${containerTexts[num].pointTwo}</li>
+  <li class= "points">${containerTexts[num].pointThree}</li>
+  </ul>`;
+};
+
+//
+const navDisplay = () => {
+  navContents.forEach((elem) => {
+    elem.classList.toggle("nav-toggle");
+  });
+  navContainer.classList.toggle("nav-background");
+  burgerMenu.classList.toggle("burger-filter");
+};
+
+const renderContainerItems = (key, num) => {
   const domKeys = [
     { ytBox: "project-container-info-a", num: 0 },
     { spBox: "project-container-info-b", num: 1 },
@@ -32,34 +81,6 @@ const containerDisplay = (key, num) => {
   }
 };
 
-const elementAppend = (num) => {
-  const containerTexts = [
-    ["skills learned blah blah blah"],
-    ["playing around with js or bs"],
-    ["something abt getting introduced to js"],
-  ];
-  //console.log(containerTexts[num]);
-
-  const parentElement = document.querySelectorAll(".sub-container-3-box");
-  //console.log(parentElement[num]);
-  parentElement[
-    num
-  ].innerHTML += `<p class= 'bullet-points'>${containerTexts[num]}</p>`;
-};
-
-//
-const navDisplay = () => {
-  navContents.forEach((elem) => {
-    elem.classList.toggle("nav-toggle");
-  });
-  navContainer.classList.toggle("nav-background");
-  burgerMenu.classList.toggle("burger-filter");
-};
-
-const renderContainerItems = (key, num) => {
-  containerDisplay(key, num);
-};
-
 const containerEventAction = (container, key, num) => {
   let toggled = true;
   toggled = !toggled;
@@ -78,9 +99,11 @@ fccContainerText.addEventListener("click", () =>
   containerEventAction(fccBox, "fccBox", 2)
 );
 
-burgerMenu.addEventListener("mouseover", () =>
+// idk how to not make mouseout not glitch the thingy
+// for now fix other stuff b4 trying mouseover
+burgerMenu.addEventListener("click", () =>
   containerEventAction(burgerMenu, "burgerMenu", 3)
 );
-// navContainer.addEventListener("mouseout", () =>
+// navSubContainer.addEventListener("mouseout", () =>
 //   containerEventAction(burgerMenu, "burgerMenu", 3)
 // );
