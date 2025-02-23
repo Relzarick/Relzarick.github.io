8eef03a00551bacd4aa4169d40faecb00f805b3f
Relzarick <156683271+Relzarick@users.noreply.github.com>
2025-02-23 10:47:05 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const projectContainer = document.querySelector(".main-container-3");
 
 const renderContainerItems = (id, container) => {
   const domKeys = {
-    p1: "<div class='v-line fade-in'></div><P class='fade-in'>Gained experience in creating layout structures with HTML and organizing content. Developed CSS styling skills, including color, font, and element positioning.</p>",
-    p2: "<div class='v-line fade-in'><p class='fade-in'>Learnt the basics of JavaScript syntax, including variables, data types, and operators. Learnt about scope and variable declaration. Gained an understanding of functions and how to create and call them.</p>",
-    p3: "<div class='v-line fade-in'><p class='fade-in'>Started to really get into the complexity of JavaScript. Enhanced problem-solving by debugging and refining code for design and functionality. Expanded my understanding of JavaScript events, handling user interactions like clicks and keyboard inputs.</p>",
-    p4: "<div class='v-line fade-in'><p class='fade-in'>Became familiar with scope, closures, and how they affect variable access and function execution. Explored objects and arrays in-depth, understanding how to store and manipulate data.</p>",
+    p1: "<div class='v-line fade-in'></div><p class='fade-in'>Gained experience in creating layout structures with HTML and organizing content. Developed CSS styling skills, including color, font, and element positioning.</p>",
+    p2: "<div class='v-line fade-in'></div><p class='fade-in'>Learnt the basics of JavaScript syntax, including variables, data types, and operators. Learnt about scope and variable declaration. Gained an understanding of functions and how to create and call them.</p>",
+    p3: "<div class='v-line fade-in'></div><p class='fade-in'>Started to really get into the complexity of JavaScript. Enhanced problem-solving by debugging and refining code for design and functionality. Expanded my understanding of JavaScript events, handling user interactions like clicks and keyboard inputs.</p>",
+    p4: "<div class='v-line fade-in'></div><p class='fade-in'>Became familiar with scope, closures, and how they affect variable access and function execution. Explored objects and arrays in-depth, understanding how to store and manipulate data.</p>",
   };
 
   console.log(domKeys[id]);
@@ -15,7 +15,6 @@ const renderContainerItems = (id, container) => {
   container.innerHTML += domKeys[id];
 };
 
-// ! Something wong here on mobile view
 projectContainer.addEventListener("click", (event) => {
   // * Finds the nearest ancestor element
   const container = event.target.closest(".inner-sub-c3");
@@ -30,9 +29,14 @@ projectContainer.addEventListener("click", (event) => {
 const pc = document.querySelector(".pc");
 const mobile = document.querySelector(".phone");
 const screenWidth = window.matchMedia("(max-width: 460px)");
+console.log(screenWidth);
+
+const popUp = () => {};
+
 if (screenWidth.matches) {
   pc.style.display = "none";
   mobile.style.display = "block";
+  popUp();
 } else {
   mobile.style.display = "none";
   pc.style.display = "block";
