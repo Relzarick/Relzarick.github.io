85290089c44f0e831c43970b51c528ce9752213b
Relzarick <156683271+Relzarick@users.noreply.github.com>
2025-02-26 12:34:04 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const renderContainerItems = (id, container) => {
   const domKeys = {
     p1: "<div class='v-line fade-in'></div><p class='fade-in'>Gained experience in creating layout structures with HTML and organizing content. Developed CSS styling skills, including color, font, and element positioning.</p>",
     p2: "<div class='v-line fade-in'></div><p class='fade-in'>Learned the basics of JavaScript syntax, including variables, data types, and operators. Learnt about scope and variable declaration. Gained an understanding of functions and how to create and call them.</p>",
-    p3: "<div class='v-line fade-in'></div><p class='fade-in'>Started to really get into the complexity of JavaScript. Enhanced problem-solving by debugging and refining code for design and functionality. Expanded my understanding of JavaScript events, handling user interactions like clicks and keyboard inputs.</p>",
+    p3: "<div class='v-line fade-in'></div><p class='fade-in'>Started to really get into the complexities of JavaScript. Enhanced problem-solving by debugging and refining code for design and functionality. Expanded my understanding of JavaScript events, handling user interactions like clicks and keyboard inputs.</p>",
     p4: "<div class='v-line fade-in'></div><p class='fade-in'>Became familiar with scope, closures, and how they affect variable access and function execution. Explored objects and arrays in-depth, understanding how to store and manipulate data.</p>",
   };
 
